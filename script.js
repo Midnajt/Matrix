2a4000867c411d75bf40c15c9df8b628d39d4c23
Marcin_Cer <midnajt0@gmail.com>
2022-04-21 13:45:12 +0200

    speed of animated control added (fps)

--- script.js
+++ script.js
@@ -15,9 +15,8 @@ class Symbol {
 
     draw(context){
         this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
-        context.fillStyle= '#0aff0a';
         context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        if(this.y * this.fontSize > this.canvasHeight){
+        if(this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98){
             this.y = 0;
         } else {
             this.y += 1;
@@ -37,24 +36,33 @@ class Effect {
     }
 
     #initialize(){
-        // console.log(this.columns)
-        console.log(this.columns.length)
-        // console.log('1')
         for(let i = 0; i < this.columns; i++){
             this.symbols[i] = new Symbol(i,0,this.fontSize,this.canvasHeight);
         }
-        // console.log('2')
     }
 }
 
 const effect = new Effect(canvas.width,canvas.height);
+let lastTime = 0;
+const fps = 15; // here we can control how was animate is generated
+const nextFrame = 1000/fps;
+let timer = 0;
 
-function animate(){
-    ctx.fillStyle = 'rgba(0,0,0,0.05)';
-    ctx.fillRect(0,0, canvas.width, canvas.height)
-    ctx.font = effect.fontSize + 'px monospace';
-    effect.symbols.forEach(symbol => symbol.draw(ctx));
+function animate(timeStamp){
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    if(timer > nextFrame ){
+        ctx.fillStyle = 'rgba(0,0,0,0.05)';
+        ctx.textAlign = 'center';
+        ctx.fillRect(0,0, canvas.width, canvas.height)
+        ctx.fillStyle= '#0aff0a';
+        ctx.font = effect.fontSize + 'px monospace';
+        effect.symbols.forEach(symbol => symbol.draw(ctx));
+        timer = 0;
+    } else {
+        timer += deltaTime;
+    }
     requestAnimationFrame(animate);
 }
 
-animate();
+animate(0);
