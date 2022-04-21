0656440630a6a4237c3bc7e0ea7878b37ba04e1a
Marcin_Cer <midnajt0@gmail.com>
2022-04-21 13:48:54 +0200

    resize window solution added

--- script.js
+++ script.js
@@ -40,6 +40,14 @@ class Effect {
             this.symbols[i] = new Symbol(i,0,this.fontSize,this.canvasHeight);
         }
     }
+
+    resize(width, height){
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+        this.columns = this.canvasWidth/this.fontSize;
+        this.symbols = [];
+        this.#initialize();
+    }
 }
 
 const effect = new Effect(canvas.width,canvas.height);
@@ -66,3 +74,9 @@ function animate(timeStamp){
 }
 
 animate(0);
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width,canvas.height);
+})
